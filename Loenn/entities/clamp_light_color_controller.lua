27551a9f2b3f6fb741e8bde9fc2c528d18aa3c40
aonkeeper4 <aonkeeper4@gmail.com>
2025-03-07 17:53:11 +0000

    tint mode wahoo

--- Loenn/entities/clamp_light_color_controller.lua
+++ Loenn/entities/clamp_light_color_controller.lua
@@ -1,20 +1,33 @@
 local clampLightColorController = {}
 
+local clampModes = {
+    "Clamp",
+    "Tint"
+}
+
 clampLightColorController.name = "aonHelper/ClampLightColorController"
 clampLightColorController.depth = 0
-clampLightColorController.texture = "objects/aonHelper/clampLightColorController"
 clampLightColorController.placements = {
     {
         name = "clampLightColorController",
         data = {
             color = "ffffff",
+            clampMode = "Clamp"
         }
     }
 }
 clampLightColorController.fieldInformation = {
     color = {
         fieldType = "color"
+    },
+    clampMode = {
+        options = clampModes,
+        editable = false
     }
 }
+
+clampLightColorController.texture = function(room, entity)
+    return "objects/aonHelper/clampLightColorController/" .. string.lower(entity.clampMode or "clamp")
+end
 
 return clampLightColorController
--- Source/Entities/ClampLightColorController.cs
+++ Source/Entities/ClampLightColorController.cs
@@ -2,6 +2,7 @@ using Monocle;
 using Microsoft.Xna.Framework;
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Celeste.Mod.aonHelper.Entities
 {
@@ -10,6 +11,12 @@ namespace Celeste.Mod.aonHelper.Entities
     public class ClampLightColorController : Entity
     {
         private Color clampColor;
+
+        public enum ClampMethod {
+            Clamp,
+            Tint,
+        }
+        private readonly ClampMethod clampMethod;
 
         // this might be wrongg but it looks fine to me
         private static readonly BlendState clampColorState = new()
@@ -20,10 +27,19 @@ namespace Celeste.Mod.aonHelper.Entities
             AlphaSourceBlend = Blend.Zero,
             AlphaDestinationBlend = Blend.One,
         };
+        private static readonly BlendState tintColorState = new()
+        {
+            ColorBlendFunction = BlendFunction.Add,
+            ColorSourceBlend = Blend.DestinationColor,
+            ColorDestinationBlend = Blend.Zero,
+            AlphaSourceBlend = Blend.Zero,
+            AlphaDestinationBlend = Blend.One,
+        };
 
         public ClampLightColorController(EntityData data, Vector2 offset) : base(data.Position + offset)
         {
             clampColor = data.HexColor("color", Color.White);
+            clampMethod = data.Enum("clampMethod", ClampMethod.Clamp);
         }
 
         public static void Load()
@@ -42,7 +58,11 @@ namespace Celeste.Mod.aonHelper.Entities
 
             if (scene.Tracker.GetEntity<ClampLightColorController>() is ClampLightColorController controller)
             {
-                Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, clampColorState);
+                Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, controller.clampMethod switch {
+                    ClampMethod.Clamp => clampColorState,
+                    ClampMethod.Tint => tintColorState,
+                    _ => throw new Exception($"Invalid color clamping method: {controller.clampMethod}"),
+                });
                 Draw.Rect(new Rectangle(0, 0, GameplayBuffers.Light.Width, GameplayBuffers.Light.Height), controller.clampColor);
                 Draw.SpriteBatch.End();
             }
