6a93e4cca4f15a971fc09372e7f55cb592c1e49b
aonkeeper4 <aonkeeper4@gmail.com>
2025-11-01 12:13:17 +0000

    add no ghost option to resizable heart

--- Loenn/entities/resizable_heart.lua
+++ Loenn/entities/resizable_heart.lua
@@ -1,22 +1,14 @@
 local drawableSprite = require("structs.drawable_sprite")
+local drawableRectangle = require("structs.drawable_rectangle")
 local utils = require("utils")
 
 local resizableHeart = {}
 
 resizableHeart.name = "aonHelper/ResizableHeart"
-
-resizableHeart.fieldInformation = {
-    color = {
-        fieldType = "color"
-    },
-    path = {
-        options = { "", "heartgem0", "heartgem1", "heartgem2", "heartgem3" }
-    },
-}
-
 resizableHeart.placements = {
     {
         name = "custom",
+        placementType = "rectangle",
         data = {
             width = 16,
             height = 16,
@@ -24,52 +16,75 @@ resizableHeart.placements = {
             path = "",
             isFake = false,
             respawnTimer = 3.0,
+            disableGhost = false
         }
     },
     {
         name = "blue",
+        placementType = "rectangle",
         data = {
             width = 16,
             height = 16,
-            color = "00a81f",
+            color = "5caefa",
             path = "heartgem0",
             isFake = false,
             respawnTimer = 3.0,
+            disableGhost = false
         }
     },
     {
         name = "red",
+        placementType = "rectangle",
         data = {
             width = 16,
             height = 16,
-            color = "00a81f",
+            color = "ff2457",
             path = "heartgem1",
             isFake = false,
             respawnTimer = 3.0,
+            disableGhost = false
         }
     },
     {
         name = "gold",
+        placementType = "rectangle",
         data = {
             width = 16,
             height = 16,
-            color = "00a81f",
+            color = "fffc24",
             path = "heartgem2",
             isFake = false,
             respawnTimer = 3.0,
+            disableGhost = false
         }
     },
     {
         name = "white",
+        placementType = "rectangle",
         data = {
             width = 16,
             height = 16,
-            color = "00a81f",
+            color = "bebdb8",
             path = "heartgem3",
             isFake = false,
             respawnTimer = 3.0,
+            disableGhost = false
         }
     }
+}
+resizableHeart.fieldInformation = {
+    color = {
+        fieldType = "color"
+    },
+    path = {
+        options = {
+            "",
+            "heartgem0",
+            "heartgem1",
+            "heartgem2",
+            "heartgem3"
+        }
+    },
 }
 
 local heartTextures = {
@@ -78,29 +93,48 @@ local heartTextures = {
     ["heartgem2"] = "collectables/heartGem/2/00",
     ["heartgem3"] = "collectables/heartGem/3/00"
 }
+local heartColors = {
+    [""] = "00a81f",
+    ["heartgem0"] = "5caefa",
+    ["heartgem1"] = "ff2457",
+    ["heartgem2"] = "fffc24",
+    ["heartgem3"] = "bebdb8"
+}
 
 local resizableHeartOutlineTexture = "collectables/aonHelper/resizableHeart_Outline/00"
 local resizableHeartTexture = "collectables/aonHelper/resizableHeart/00"
 
 function resizableHeart.sprite(room, entity)
-    local path = entity.path
+    local x, y = entity.x or 0, entity.y or 0
+    local width, height = entity.width or 16, entity.height or 16
+    
+    local path = entity.path or ""
+    local color = entity.color or heartColors[path] or heartColors["heartgem3"]
 
+    local outlineColor = utils.getColor(color)
+    local insideColor = { outlineColor[1], outlineColor[2], outlineColor[3], 0.5 }
+    local rectangle = drawableRectangle.fromRectangle("bordered", x - width / 2, y - height / 2, width, height, insideColor, outlineColor)
+    
     if path == "" then
-        local outline = drawableSprite.fromTexture(resizableHeartOutlineTexture, entity)
         local heart = drawableSprite.fromTexture(resizableHeartTexture, entity)
-        return { heart, outline }
-    end
+        local outline = drawableSprite.fromTexture(resizableHeartOutlineTexture, entity)
+        outline:setColor({ 1.0, 1.0, 1.0, 1.0 })
+        
+        return { rectangle, heart, outline }
+    else
+        local texture = heartTextures[path] or heartTextures["heartgem3"]
+        local sprite = drawableSprite.fromTexture(texture, entity)
+        sprite:setColor({ 1.0, 1.0, 1.0, 1.0 })
 
-    local texture = heartTextures[path] or heartTextures["heartgem3"]
-    local sprite = drawableSprite.fromTexture(texture, entity)
-    sprite.color = { 1.0, 1.0, 1.0, 1.0 }
-    return sprite
+        return { rectangle, sprite }
+    end
 end
 
-function resizableHeart.selection(room, entity)
+function resizableHeart.rectangle(room, entity)
     local x, y = entity.x or 0, entity.y or 0
-    local w, h = entity.width or 16, entity.height or 16
-    return utils.rectangle(x - w / 2, y - h / 2, w, h)
+    local width, height = entity.width or 16, entity.height or 16
+    
+    return utils.rectangle(x - width / 2, y - height / 2, width, height)
 end
 
 return resizableHeart
--- Source/Entities/ResizableHeart.cs
+++ Source/Entities/ResizableHeart.cs
@@ -8,69 +8,76 @@ namespace Celeste.Mod.aonHelper.Entities;
 [CustomEntity("aonHelper/ResizableHeart")]
 public class ResizableHeart : HeartGem
 {
-    private Sprite spriteOutline;
-
-    private Color color;
-
-    private readonly string spriteId;
-
     private readonly int width, height;
+    
+    private readonly string spriteID;
+    private Sprite spriteOutline;
+    private Color color;
+    private readonly bool disableGhost;
 
-    public float respawnTimer;
+    private readonly float respawnTime;
+    private float respawnTimer;
 
-    public float baseRespawnTimer = 3f;
-
-    public bool fake;
+    private readonly bool fake;
 
     public ResizableHeart(EntityData data, Vector2 offset) : base(data, offset)
     {
-        entityID = new EntityID(data.Level.Name, data.ID);
-        color = Calc.HexToColor(data.Attr("color", "00a81f"));
-        spriteId = data.Attr("path");
         width = data.Width;
         height = data.Height;
+        
+        spriteID = data.Attr("path");
+        color = data.HexColor("color", Calc.HexToColor("00a81f"));
+        disableGhost = data.Bool("disableGhost");
+        
+        respawnTime = data.Float("respawnTimer", 3f);
+        
         fake = data.Bool("isFake");
-        baseRespawnTimer = data.Float("respawnTimer", 3f);
     }
 
     public override void Awake(Scene scene)
     {
         base.Awake(scene);
-        AreaKey area = (scene as Level).Session.Area;
-        IsGhost = !IsFake && !fake && SaveData.Instance.Areas_Safe[area.ID].Modes[(int)area.Mode].HeartGem;
+
+        Level level = SceneAs<Level>();
+        
         Collider = new Hitbox(width, height, -width / 2, -height / 2);
+        
+        AreaKey area = level.Session.Area;
+        IsGhost = !IsFake && !fake && SaveData.Instance.Areas_Safe[area.ID].Modes[(int)area.Mode].HeartGem;
+        
         Remove(sprite);
-        if (!string.IsNullOrWhiteSpace(spriteId))
+        if (!string.IsNullOrWhiteSpace(spriteID))
         {
-            sprite = GFX.SpriteBank.Create(spriteId);
+            sprite = GFX.SpriteBank.Create(spriteID);
+            
             sprite.Play("spin");
-            if (IsGhost)
-            {
+            if (IsGhost && !disableGhost)
                 sprite.Color = Color.White * 0.8f;
-            }
-            switch (spriteId)
+            
+            switch (spriteID)
             {
                 case "heartgem0":
                     color = Color.Aqua;
                     shineParticle = P_BlueShine;
                     break;
+                
                 case "heartgem1":
                     color = Color.Red;
                     shineParticle = P_RedShine;
                     break;
+                
                 case "heartgem2":
                     color = Color.Gold;
                     shineParticle = P_GoldShine;
                     break;
+                
                 case "heartgem3":
                     color = Calc.HexToColor("dad8cc");
                     shineParticle = P_FakeShine;
                     break;
+                
                 default:
-                    shineParticle = new ParticleType(P_BlueShine)
-                    {
-                        Color = color
-                    };
+                    shineParticle = new ParticleType(P_BlueShine) { Color = color };
                     break;
             }
         }
@@ -78,39 +85,27 @@ public class ResizableHeart : HeartGem
         {
             sprite = aonHelperModule.SpriteBank.Create("aonHelper_resizableHeart");
             spriteOutline = aonHelperModule.SpriteBank.Create("aonHelper_resizableHeartOutline");
-            sprite.Color = IsGhost && !fake ? (Color.Lerp(color, Color.White, 0.8f) * 0.8f) : color;
-            shineParticle = new ParticleType(P_BlueShine)
-            {
-                Color = color
-            };
+            sprite.Color = IsGhost && !fake && !disableGhost ? Color.Lerp(color, Color.White, 0.8f) * 0.8f : color;
+            shineParticle = new ParticleType(P_BlueShine) { Color = color };
         }
-        sprite.OnLoop = delegate (string anim)
+        sprite.OnLoop = anim =>
         {
-            if (Visible && anim == "spin" && autoPulse)
-            {
-                if (IsFake)
-                {
-                    Audio.Play("event:/new_content/game/10_farewell/fakeheart_pulse", Position);
-                }
-                else
-                {
-                    Audio.Play("event:/game/general/crystalheart_pulse", Position);
-                }
-                ScaleWiggler.Start();
-                (base.Scene as Level).Displacement.AddBurst(Position, 0.35f, 8f, 48f, 0.25f);
-            }
+            if (!Visible || anim != "spin" || !autoPulse)
+                return;
+            
+            Audio.Play(IsFake ? "event:/new_content/game/10_farewell/fakeheart_pulse" : "event:/game/general/crystalheart_pulse", Position);
+
+            ScaleWiggler.Start();
+            level.Displacement.AddBurst(Position, 0.35f, 8f, 48f, 0.25f);
         };
-        Remove(ScaleWiggler);
-        ScaleWiggler = Wiggler.Create(0.5f, 4f, delegate (float f)
-        {
-            sprite.Scale = Vector2.One * (1f + f * 0.25f);
-        });
-        Add(ScaleWiggler);
         Add(sprite);
-        if (spriteOutline != null)
-        {
+        if (spriteOutline is not null)
             Add(spriteOutline);
-        }
+        
+        Remove(ScaleWiggler);
+        ScaleWiggler = Wiggler.Create(0.5f, 4f, f => sprite.Scale = Vector2.One * (1f + f * 0.25f));
+        Add(ScaleWiggler);
+        
         light.Color = Color.Lerp(color, Color.White, 0.5f);
     }
 
@@ -118,6 +113,7 @@ public class ResizableHeart : HeartGem
     {
         if (fake)
             collected = respawnTimer > 0f;
+        
         base.Update();
 
         if (respawnTimer > 0f)
@@ -129,7 +125,8 @@ public class ResizableHeart : HeartGem
                 ScaleWiggler.Start();
             }
         }
-        if (spriteOutline != null)
+        
+        if (spriteOutline is not null)
         {
             spriteOutline.Position = sprite.Position;
             spriteOutline.Scale = sprite.Scale;
@@ -141,92 +138,82 @@ public class ResizableHeart : HeartGem
         }
     }
 
-    public void CollectFake(Player player, float angle)
+    private void CollectFake(Player player, float angle)
     {
-        if (Collidable)
-        {
-            Collidable = Visible = false;
-            respawnTimer = baseRespawnTimer;
-            Celeste.Freeze(0.05f);
-            SceneAs<Level>().Shake();
-            SlashFx.Burst(Position, angle);
-            player?.RefillDash();
-        }
+        if (!Collidable)
+            return;
+        
+        Collidable = Visible = false;
+        respawnTimer = respawnTime;
+        
+        Celeste.Freeze(0.05f);
+        SceneAs<Level>().Shake();
+        SlashFx.Burst(Position, angle);
+        
+        player?.RefillDash();
     }
-    private static void mod_HeartGemOnHoldable(On.Celeste.HeartGem.orig_OnHoldable orig, HeartGem self, Holdable h)
+    
+    #region Hooks
+    
+    internal static void Load()
     {
-        if (self is ResizableHeart resizable)
-        {
-            Player entity = resizable.Scene.Tracker.GetEntity<Player>();
-            if (resizable.fake)
-            {
-
-                if (resizable.Visible && h.Dangerous(resizable.holdableCollider))
-                {
-                    resizable.CollectFake(entity, h.GetSpeed().Angle());
-                }
-            }
-            else
-            {
-                orig(self, h);
-            }
-        }
-        else
-        {
-            orig(self, h);
-        }
+        On.Celeste.HeartGem.OnPlayer += HeartGem_OnPlayer;
+        On.Celeste.HeartGem.OnHoldable += HeartGem_OnHoldable;
     }
 
-    private static void mod_HeartGemOnPlayer(On.Celeste.HeartGem.orig_OnPlayer orig, HeartGem self, Player player)
+    internal static void Unload()
     {
-        if (self is ResizableHeart resizable)
+        On.Celeste.HeartGem.OnPlayer -= HeartGem_OnPlayer;
+        On.Celeste.HeartGem.OnHoldable -= HeartGem_OnHoldable;
+    }
+    
+    private static void HeartGem_OnHoldable(On.Celeste.HeartGem.orig_OnHoldable orig, HeartGem self, Holdable holdable)
+    {
+        if (self is not ResizableHeart resizable)
         {
-            if (resizable.fake)
-            {
-                if (!resizable.Visible || (resizable.Scene as Level).Frozen)
-                {
-                    return;
-                }
-                if (player.DashAttacking)
-                {
-                    Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
-                    resizable.CollectFake(player, player.Speed.Angle());
-                    return;
-                }
-                if (resizable.bounceSfxDelay <= 0f)
-                {
-                    Audio.Play("event:/game/general/crystalheart_bounce", resizable.Position);
-                    resizable.bounceSfxDelay = 0.1f;
-                }
-                player.PointBounce(resizable.Center);
-                resizable.moveWiggler.Start();
-                resizable.ScaleWiggler.Start();
-                resizable.moveWiggleDir = (resizable.Center - player.Center).SafeNormalize(Vector2.UnitY);
-                Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
-            }
-            else
-            {
-                orig(self, player);
-            }
+            orig(self, holdable);
+            return;
         }
+        
+        Player entity = resizable.Scene.Tracker.GetEntity<Player>();
+        if (resizable.fake && resizable.Visible && holdable.Dangerous(resizable.holdableCollider))
+            resizable.CollectFake(entity, holdable.GetSpeed().Angle());
         else
+            orig(self, holdable);
+    }
+
+    private static void HeartGem_OnPlayer(On.Celeste.HeartGem.orig_OnPlayer orig, HeartGem self, Player player)
+    {
+        if (self is not ResizableHeart { fake: true } resizable)
         {
             orig(self, player);
+            return;
         }
-    }
 
-    public static void Load()
-    {
-        // On.Celeste.HeartGem.Collect += modCollect;
-        On.Celeste.HeartGem.OnPlayer += mod_HeartGemOnPlayer;
-        On.Celeste.HeartGem.OnHoldable += mod_HeartGemOnHoldable;
+        Level level = resizable.SceneAs<Level>();
+        
+        if (!resizable.Visible || level.Frozen)
+            return;
+        
+        if (player.DashAttacking)
+        {
+            Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
+            resizable.CollectFake(player, player.Speed.Angle());
+            return;
+        }
+        
+        player.PointBounce(resizable.Center);
+        if (resizable.bounceSfxDelay <= 0f)
+        {
+            Audio.Play("event:/game/general/crystalheart_bounce", resizable.Position);
+            resizable.bounceSfxDelay = 0.1f;
+        }
+        resizable.moveWiggler.Start();
+        resizable.ScaleWiggler.Start();
+        resizable.moveWiggleDir = (resizable.Center - player.Center).SafeNormalize(Vector2.UnitY);
+        
+        Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
     }
-
-    public static void Unload()
-    {
-
-        // On.Celeste.HeartGem.Collect -= modCollect;
-        On.Celeste.HeartGem.OnPlayer -= mod_HeartGemOnPlayer;
-        On.Celeste.HeartGem.OnHoldable -= mod_HeartGemOnHoldable;
-    }
+    
+    #endregion
 }
