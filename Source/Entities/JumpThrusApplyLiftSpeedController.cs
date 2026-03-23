3afb417788534ea3c2e277d089468f51ac7887eb
aonkeeper4 <aonkeeper4@gmail.com>
2026-03-23 22:15:42 +0000

    i think this should be right now

--- Source/Entities/JumpThrusApplyLiftSpeedController.cs
+++ Source/Entities/JumpThrusApplyLiftSpeedController.cs
@@ -35,9 +35,26 @@ public class JumpThrusApplyLiftSpeedController(Vector2 position, string flag) : 
         ILCursor cursor = new(il);
 
         // due to where we hook, the `actor` local is uninitialised the first time we access it, which is technically UB
-        // let's initialise it so we dont like idk corrupt memory or whatever
+        // let's initialise it so we don't like idk corrupt memory or whatever
         cursor.EmitLdnull();
         cursor.EmitStloc1();
+        
+        /*
+         * IL_0020: ldloca.s 0
+         * IL_0022: call instance !0 valuetype [mscorlib]System.Collections.Generic.List`1/Enumerator<class Monocle.Entity>::get_Current()
+         * IL_0027: castclass Celeste.Actor
+         * IL_002c: stloc.1
+         */
+        if (!cursor.TryGotoNextBestFit(MoveType.After,
+            instr => instr.MatchLdloca(0),
+            instr => instr.MatchCall<List<Entity>.Enumerator>("get_Current"),
+            instr => instr.MatchCastclass<Actor>(),
+            instr => instr.MatchStloc1()))
+            throw new HookHelper.HookException(il, "Unable to find beginning of loop to modify.");
+        
+        cursor.EmitLdarg0();
+        cursor.EmitLdloc1();
+        cursor.EmitDelegate(PreApplyLiftSpeed);
         
         /*
          * IL_005c: ldloca.s 0
@@ -52,21 +69,37 @@ public class JumpThrusApplyLiftSpeedController(Vector2 position, string flag) : 
 
         cursor.EmitLdarg0();
         cursor.EmitLdloc1();
-        cursor.EmitDelegate(ApplyLiftSpeed);
+        cursor.EmitDelegate(PostApplyLiftSpeed);
 
         return;
 
-        static void ApplyLiftSpeed(JumpThru jumpThru, Actor actor)
-        {
+        static bool ShouldApplyLiftSpeed(JumpThru jumpThru, Actor actor) {
             Level level = jumpThru.SceneAs<Level>();
             if (level.Tracker.GetEntity<JumpThrusApplyLiftSpeedController>() is not { } controller
                 || !level.Session.GetFlag(controller.flag) && controller.flag is not null)
-                return;
+                return false;
             
             if (actor is null || !actor.IsRiding(jumpThru))
+                return false;
+
+            return true;
+        }
+
+        static void PreApplyLiftSpeed(JumpThru jumpThru, Actor actor)
+        {
+            if (!ShouldApplyLiftSpeed(jumpThru, actor))
+                return;
+
+            jumpThru.Collidable = false;
+        }
+        
+        static void PostApplyLiftSpeed(JumpThru jumpThru, Actor actor)
+        {
+            if (!ShouldApplyLiftSpeed(jumpThru, actor))
                 return;
             
             actor.LiftSpeed = jumpThru.LiftSpeed;
+            jumpThru.Collidable = true;
         }
     }
 
