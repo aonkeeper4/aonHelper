fe54b02f90915041e9cee035a04a81766cb1fe3e
aonkeeper4 <aonkeeper4@gmail.com>
2026-03-30 22:53:34 +0100

    improve dependency handling

--- Source/aonHelperDependencies.cs
+++ Source/aonHelperDependencies.cs
@@ -19,11 +19,14 @@ public static class aonHelperDependencies
     internal static bool ReverseHelperLoaded;
     
     #endregion
+
+    private delegate bool ShouldLoadDependencyHandler(EverestModule module);
+    private delegate void LoadDependencyHandler();
+    private delegate void UnloadDependencyHandler();
     
-    private delegate bool DependencyLoadHandler(EverestModule module);
-    private delegate void DependencyUnloadHandler();
-    
-    private class DependencyState(Action<bool> setLoaded, Func<bool> getLoaded, DependencyLoadHandler load, DependencyUnloadHandler unload)
+    private class DependencyState(
+        Action<bool> setLoaded, Func<bool> getLoaded,
+        ShouldLoadDependencyHandler shouldLoadDependency, LoadDependencyHandler loadDependency, UnloadDependencyHandler unloadDependency)
     {
         public bool Loaded
         {
@@ -31,42 +34,46 @@ public static class aonHelperDependencies
             set => setLoaded(value);
         }
 
-        public readonly DependencyLoadHandler Load = load;
-        public readonly DependencyUnloadHandler Unload = unload;
+        public readonly ShouldLoadDependencyHandler ShouldLoadDependency = shouldLoadDependency;
+        public readonly LoadDependencyHandler LoadDependency = loadDependency;
+        public readonly UnloadDependencyHandler UnloadDependency = unloadDependency;
     }
     private static readonly Dictionary<EverestModuleMetadata, DependencyState> Dependencies = new()
     {
-        { DzhakeHelper, new DependencyState(loaded => DzhakeHelperLoaded = loaded, () => DzhakeHelperLoaded, null, null) },
-        { ReverseHelper, new DependencyState(loaded => ReverseHelperLoaded = loaded, () => ReverseHelperLoaded, null, null) }
+        { DzhakeHelper, new DependencyState(loaded => DzhakeHelperLoaded = loaded, () => DzhakeHelperLoaded, null, null, null) },
+        { ReverseHelper, new DependencyState(loaded => ReverseHelperLoaded = loaded, () => ReverseHelperLoaded, null, null, null) }
     };
+    
+    private static void UpdateDependencies(bool load)
+    {
+        foreach ((EverestModuleMetadata metadata, DependencyState state) in Dependencies)
+        {
+            EverestModule module = null;
+            bool shouldLoad = load
+                && Everest.Loader.TryGetDependency(metadata, out module)
+                && (state.ShouldLoadDependency?.Invoke(module!) ?? true);
+            if (shouldLoad == state.Loaded)
+                continue;
+
+            if (shouldLoad)
+            {
+                state.LoadDependency?.Invoke();
+                state.Loaded = true;
+                Logger.Info(LogID, $"Registered support for {metadata.Name} (version {module!.Metadata.Version.ToString()}).");
+            }
+            else
+            {
+                state.UnloadDependency?.Invoke();
+                state.Loaded = false;
+                Logger.Info(LogID, $"Unregistered support for {metadata.Name}.");
+            }
+        }
+    }
     
     // we need to make sure these are loaded *before* any hooks are loaded and *after* all hooks are unloaded, so we don't use the mod lifecycle attributes
     internal static void Load()
-    {
-        foreach ((EverestModuleMetadata metadata, DependencyState state) in Dependencies)
-        {
-            if (state.Loaded || !Everest.Loader.TryGetDependency(metadata, out EverestModule module))
-                continue;
-            
-            bool loaded = state.Load?.Invoke(module) ?? true;
-            state.Loaded = loaded;
-                
-            if (loaded)
-                Logger.Info(LogID, $"Registered support for {metadata.Name} (version {metadata.Version}).");
-        }
-    }
+        => UpdateDependencies(true);
     
     internal static void Unload()
-    {
-        foreach ((EverestModuleMetadata metadata, DependencyState state) in Dependencies)
-        {
-            if (!state.Loaded)
-                continue;
-            
-            state.Unload?.Invoke();
-            state.Loaded = false;
-            
-            Logger.Info(LogID, $"Unregistered support for {metadata.Name}.");
-        }
-    }
+        => UpdateDependencies(false);
 }
