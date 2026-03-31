7576e309fede7ffbcbd6210a4426c7bb5c163795
aonkeeper4 <aonkeeper4@gmail.com>
2026-03-31 09:17:54 +0100

    fix reversehelper interop ??

--- Source/Imports/ReverseHelper.cs
+++ Source/Imports/ReverseHelper.cs
@@ -3,14 +3,14 @@ using System.Collections.Immutable;
 
 namespace Celeste.Mod.aonHelper.Imports;
 
-// todo: kill reversehelper for existing
+// growls
 [GenerateImports("ReverseHelper.DreamBlock")]
 public static partial class ReverseHelper
 {
     public static partial void RegisterDreamBlockLike(Type type, Action<Entity> activate, Action<Entity> deactivate);
     public static partial void RegisterDreamBlockDummy(Type type, Func<Entity, Entity> getDummy);
     
-    public static partial bool PlayerHasDreamDash(Entity dreamBlockLike, Func<bool> fallback = null);
+    public static partial bool PlayerHasDreamDash(Entity dreamBlockLike);
     
     public static partial long ConfigureGetEnum(string enumVariant);
     public static partial bool? ConfigureGetFromEnum(Entity dreamBlockLike, long enumValue);
