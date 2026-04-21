5df8df2acc48a7727557a0f4538e040572767a46
aonkeeper4 <aonkeeper4@gmail.com>
2026-04-22 00:57:31 +0100

    zzzzzzzzzzzzz

--- Source/Helpers/GlobalHelper.cs
+++ Source/Helpers/GlobalHelper.cs
@@ -2,7 +2,13 @@ namespace Celeste.Mod.aonHelper.Helpers;
 
 public static class GlobalHelper
 {
-    
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class GlobalEntityAttribute(string entitySID, string onlyGlobalIf = null) : Attribute
+    {
+        public string EntitySID = entitySID;
+        
+        public string OnlyGlobalIf = onlyGlobalIf;
+    }
     
     public static void ProcessGlobalEntityAttributes(Type type, ref int attributesProcessed)
     {
